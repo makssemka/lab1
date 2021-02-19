58057ebf446ce8d390d6ab5985b7732862865f17
makssemka <makssemka23@gmail.com>
2021-02-20 01:32:14 +0300

    second

--- lab1.py
+++ lab1.py
@@ -1,1 +1,15 @@
-print("im maks")
+fib1 = 1
+fib2 = 1
+
+n =int(input("enter number Fibonachi: "))
+
+i=0
+while i < n - 2:
+    fibsum = fib1 + fib2
+    fib1 = fib2
+    fib2 = fibsum
+    i += 1
+
+print(fib2)
+
+

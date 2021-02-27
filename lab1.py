971f3048472759b0f38e358ddac4a6d147e18a28
makssemka <makssemka23@gmail.com>
2021-02-27 12:04:51 +0300

    fixed linter issues

--- lab1.py
+++ lab1.py
@@ -1,4 +1,5 @@
 import logging
+
 logging.basicConfig(level=logging.INFO)
 
 
@@ -7,19 +8,18 @@ def main():
     fib2 = 1
     while True:
         try:
-            n = int(input('input number: '))
+            n = int(input("input number: "))
             break
         except ValueError:
-            logging.info('Invalid type! Please try again.')
-    i=0
+            logging.error("Invalid type! Please try again.")
+    i = 0
     while i < n - 2:
         fibsum = fib1 + fib2
         fib1 = fib2
         fib2 = fibsum
         i += 1
     logging.info(fib2)
-    return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
